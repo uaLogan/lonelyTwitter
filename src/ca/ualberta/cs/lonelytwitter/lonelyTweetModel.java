dc346e87d083f05d55448c6b4b2629193e711450
uaLogan <lglin@cs.ualberta.ca>
2014-01-14 17:46:38 -0700

    Added constructor.

--- src/ca/ualberta/cs/lonelytwitter/lonelyTweetModel.java
+++ src/ca/ualberta/cs/lonelytwitter/lonelyTweetModel.java
@@ -1,16 +1,31 @@
 package ca.ualberta.cs.lonelytwitter;
 
+import java.util.Date;
 
 public class lonelyTweetModel
 {
 	private String text;
 	private Date timestamp;
 	
+	public lonelyTweetModel(String text, Date timestamp)
+	{
+		super();
+		this.text = text;
+		this.timestamp = timestamp;
+	}	
+	
+	public lonelyTweetModel(String text)
+	{
+		super();
+		this.text = text;
+		this.timestamp = new Date();
+	}
+
 	public String getText()
 	{
 		return text;
 	}
-	
+
 	public void setText(String text)
 	{
 		this.text = text;
