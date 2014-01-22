e20fcf04964906df84cf2cc570c26d87ef72290b
uaLogan <lglin@cs.ualberta.ca>
2014-01-21 19:49:30 -0700

    Just one more problem.. how to gracefully attempt to fromJson empty files.

--- README.md
+++ README.md
@@ -4,3 +4,13 @@ lonelyTwitter
 Lonely Twitter demo for CSCI 301
 
 This code is only for demonstrating Eclipse, Android and jUnit development.
+
+
+
+
+
+////////////////////////
+USEFUL LINKS
+
+http://stackoverflow.com/questions/8749570/why-does-adapter-add-crash
+
--- src/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
+++ src/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
@@ -6,13 +6,21 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonSyntaxException;
+
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -29,6 +37,8 @@ public class LonelyTwitterActivity extends Activity {
 	private ArrayList<String> tweets = new ArrayList<String>();
 	private String textToAdd = "";
 	
+	private Gson gson;
+	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -43,12 +53,10 @@ public class LonelyTwitterActivity extends Activity {
 		saveButton.setOnClickListener(new View.OnClickListener() {
 
 			public void onClick(View v) {
-				//setResult(RESULT_OK);
 				textToAdd = bodyText.getText().toString();
-				saveInFile(textToAdd, new Date(System.currentTimeMillis()));
-				//finish();
 				textToAdd = formatNewTweet(textToAdd);
 				adapter.add(textToAdd);
+				saveInFile();
 				adapter.notifyDataSetChanged();
 			}
 		});
@@ -65,17 +73,18 @@ public class LonelyTwitterActivity extends Activity {
 							Context.MODE_PRIVATE);
 				} catch (FileNotFoundException e)
 				{
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
 		});
+		
+		gson = new GsonBuilder().create();
 	}
 
 	@Override
 	protected void onResume() {
 		super.onStart();
-		tweets = new ArrayList(Arrays.asList(loadFromFile())); //loadFromFile call is here*
+		loadFromFile();
 		adapter = new ArrayAdapter<String>(this,
 				R.layout.list_item, tweets);
 		oldTweetsList.setAdapter(adapter);
@@ -87,15 +96,37 @@ public class LonelyTwitterActivity extends Activity {
 		return date.toString() + " | " + text;
 	}
 
-	private String[] loadFromFile() {
-		ArrayList<String> tweets = new ArrayList<String>();
+	private void loadFromFile() {
+		//ArrayList<String> tweets = new ArrayList<String>();
 		try {
 			FileInputStream fis = openFileInput(FILENAME);
-			BufferedReader in = new BufferedReader(new InputStreamReader(fis));
-			String line = in.readLine();
+			InputStreamReader isr = new InputStreamReader(fis);
+			BufferedReader in = new BufferedReader(isr);
+			
+/*			String line = in.readLine();
 			while (line != null) {
 				tweets.add(line);
 				line = in.readLine();
+			}*/
+			
+			try
+			{
+				tweets = gson.fromJson(in, ArrayList.class);
+			}
+			catch(JsonIOException e)
+			{
+				Log.d("MyTag", "qqqqqq");
+				e.printStackTrace();
+			}
+			catch(JsonSyntaxException e)
+			{
+				Log.d("MyTag", "qqqqqqqq");
+				e.printStackTrace();
+			}
+			catch(JsonParseException e)
+			{
+				Log.d("MyTag", "qqqqqqqqqq");
+				e.printStackTrace();
 			}
 
 		} catch (FileNotFoundException e) {
@@ -105,16 +136,19 @@ public class LonelyTwitterActivity extends Activity {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return tweets.toArray(new String[tweets.size()]);
+		//return tweets.toArray(new String[tweets.size()]);
 	}
 	
-	private void saveInFile(String text, Date date) {
+	private void saveInFile() {
 		try {
-			FileOutputStream fos = openFileOutput(FILENAME,
-					Context.MODE_APPEND);
-			fos.write(new String(date.toString() + " | " + text + "\n")
-					.getBytes());
+			
+			FileOutputStream fos = openFileOutput(FILENAME, Context.MODE_PRIVATE);
+			//String textToWrite = date.toString() + " | " + text + "\n";
+			OutputStreamWriter osw = new OutputStreamWriter(fos);
+			gson.toJson(tweets, osw);
+			osw.close();
 			fos.close();
+			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
