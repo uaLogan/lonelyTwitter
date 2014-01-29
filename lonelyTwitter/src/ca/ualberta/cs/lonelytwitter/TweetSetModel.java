6c9c2990afe3ccfa72f99f1d1e352e8a3fbde959
uaLogan <lglin@cs.ualberta.ca>
2014-01-28 19:14:19 -0700

    Added exception thing to addTweet

--- lonelyTwitter/src/ca/ualberta/cs/lonelytwitter/TweetSetModel.java
+++ lonelyTwitter/src/ca/ualberta/cs/lonelytwitter/TweetSetModel.java
@@ -1,18 +1,33 @@
 package ca.ualberta.cs.lonelytwitter;
 
+import java.util.ArrayList;
+
 
 public class TweetSetModel
 {
 	private int count = 0;
+	
+	ArrayList<LonelyTweetModel> tweets = new ArrayList<LonelyTweetModel>();
 
 	public Object countTweets()
 	{
 		return count;
 	}
 
-	public void addTweet(NormalTweetModel normalTweetModel)
+	public void addTweet(NormalTweetModel normalTweetModel) throws IllegalArgumentException
 	{
+		for(LonelyTweetModel tweet : tweets)
+		{
+			if(tweet.equals(normalTweetModel))
+				throw new IllegalArgumentException("already added this tweet!");
+		}
+		
+		tweets.add(normalTweetModel);
 		count++;
 	}
 
+	public LonelyTweetModel[] getTweets()
+	{
+		return tweets.toArray(new LonelyTweetModel[0]);
+	}
 }
--- lonelyTwitterTest/src/ca/ualberta/cs/lonelytwitter/test/TweetSetModelTest.java
+++ lonelyTwitterTest/src/ca/ualberta/cs/lonelytwitter/test/TweetSetModelTest.java
@@ -17,51 +17,94 @@ public class TweetSetModelTest extends ActivityInstrumentationTestCase2<LonelyTw
 		super(LonelyTwitterActivity.class);
 	}
 	
-	public void testCount()
+//	public void testCount()
+//	{
+//		/*
+//		TweetSetModel tweets = new TweetSetModel();
+//		
+//		assertEquals("tweet set should start empty", 0, tweets.countTweets());
+//		
+//		tweets.addTweet(new NormalTweetModel("test"));
+//		
+//		assertEquals("added a tweet, count should be one", 1, tweets.countTweets());
+//		*/
+//		
+//		ImportantTweetModel tweet1 = new ImportantTweetModel("test");
+//		
+//		try
+//		{
+//			Thread.sleep(2000);
+//		} catch (InterruptedException e)
+//		{
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		
+//		ImportantTweetModel tweet2 = new ImportantTweetModel("test");
+//		
+//		//different time, same text
+//		assertEquals("different timestamps", false, tweet1.equals(tweet2));
+//		
+//		//same time, same text
+//		tweet2.setTimestamp(tweet1.getTimestamp());
+//		assertEquals("same timestamps, same text", true, tweet1.equals(tweet2));
+//		
+//		//same time, different text
+//		tweet2.setText("aaaa");
+//		assertEquals("different text", false, tweet1.equals(tweet2));
+//		
+//		//different time, different text
+//		ImportantTweetModel tweet3= new ImportantTweetModel("aaaa");
+//		assertEquals("different both", false, tweet3.equals(tweet2));
+//		
+//		ImportantTweetModel tweet4 = new ImportantTweetModel("a");
+//		NormalTweetModel tweet5 = new NormalTweetModel("a");
+//		tweet4.setTimestamp(tweet5.getTimestamp());
+//		
+//		assertEquals("different class", false, tweet4.equals(tweet5));
+//	}
+
+	
+	public void testGetTweets()
 	{
-		/*
-		TweetSetModel tweets = new TweetSetModel();
+		TweetSetModel setModel = new TweetSetModel();
 		
-		assertEquals("tweet set should start empty", 0, tweets.countTweets());
+		NormalTweetModel tweet1 = new NormalTweetModel("a");
+		setModel.addTweet(tweet1);
 		
-		tweets.addTweet(new NormalTweetModel("test"));
+		LonelyTweetModel tweets[] = setModel.getTweets();
 		
-		assertEquals("added a tweet, count should be one", 1, tweets.countTweets());
-		*/
+		assertEquals("one tweet in list", tweets.length, 1);
+		assertEquals("same tweet", true, tweets[0].equals(tweet1));
+		assertEquals("same tweet", true, tweet1.equals(tweets[0]));
 		
-		ImportantTweetModel tweet1 = new ImportantTweetModel("test");
+		NormalTweetModel tweet2 = new NormalTweetModel("b");
+		NormalTweetModel tweet3 = new NormalTweetModel("c");
 		
-		//stall for time
-		double c = 0;
-		while(c < 800000000.0)
-		{
-			double a = 2.4;
-			double b = 2.6;
-			c += a*b;
-		}
+		setModel.addTweet(tweet2);
+		setModel.addTweet(tweet3);
 		
-		ImportantTweetModel tweet2 = new ImportantTweetModel("test");
+		tweets = setModel.getTweets();
 		
-		//different time, same text
-		assertEquals("different timestamps", false, tweet1.equals(tweet2));
+		assertEquals("one tweet in list", tweets.length, 3);
+		assertEquals("same tweet", true, tweets[1].equals(tweet2));
+		assertEquals("same tweet", true, tweet3.equals(tweets[2]));
 		
-		//same time, same text
-		tweet2.setTimestamp(tweet1.getTimestamp());
-		assertEquals("same timestamps, same text", true, tweet1.equals(tweet2));
-		
-		//same time, different text
-		tweet2.setText("aaaa");
-		assertEquals("different text", false, tweet1.equals(tweet2));
-		
-		//different time, different text
-		ImportantTweetModel tweet3= new ImportantTweetModel("aaaa");
-		assertEquals("different both", false, tweet3.equals(tweet2));
-		
-		ImportantTweetModel tweet4 = new ImportantTweetModel("a");
-		NormalTweetModel tweet5 = new NormalTweetModel("a");
+		NormalTweetModel tweet4 = new NormalTweetModel("d");
+		NormalTweetModel tweet5 = new NormalTweetModel("d");
 		tweet4.setTimestamp(tweet5.getTimestamp());
 		
-		assertEquals("different class", false, tweet4.equals(tweet5));
+		setModel.addTweet(tweet4);
+		
+		try
+		{
+		setModel.addTweet(tweet5);
+		fail("Expected excepton");
+		}
+		catch(IllegalArgumentException e)
+		{	
+			assertEquals("check exception message", "already added this tweet!", e.getMessage());
+		}
+		
 	}
-
 }
